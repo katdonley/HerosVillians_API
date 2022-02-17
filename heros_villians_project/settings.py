95315ee9f22e88be9b5101fad574bf52ad7b1026
Kat Donley <katdonley@Kats-MBP.lan>
2022-02-17 12:20:30 -0700

    changed database settings + created supers and super_types apps

--- heros_villians_project/settings.py
+++ heros_villians_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e_79x4gd2l=u)!p1e-#o0^rqyeor9l48rpj80!bk$er2f=d(!i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,17 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heros_villians_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -121,3 +108,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heros_villians_project.local_settings import *
+except ImportError:
+    pass
