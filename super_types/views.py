279491322f30e09d2075a6851f2e148bcb53cf05
Kat Donley <katdonley@Kats-MBP.lan>
2022-02-21 12:41:21 -0700

    created superuser and seeded supers

--- super_types/views.py
+++ super_types/views.py
@@ -1,6 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+import super_types
+from .models import SuperTypes
+from .serializers import SuperTypesSerializer
+
+
 @api_view(['GET'])
 def super_types_list(request):
-    return Response('ok')
+    super_types = SuperTypes.objects.all()
+    serializer = SuperTypesSerializer(super_types, many=True)
+    
+    return Response(serializer.data)
--- supers/views.py
+++ supers/views.py
@@ -1,6 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from super_types import serializers
+from .models import Super
+from .serializers import SuperSerializer
+
 @api_view(['GET'])
 def supers_list(request):
-    return Response('ok')
+    supers = Super.objects.all()
+    serializer = SuperSerializer(supers, many=True)
+
+    return Response(serializer.data)
